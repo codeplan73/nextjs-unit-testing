372a8327cad8178501f34336fe1425ddc0c338b6
Emmanuel Omonzebaguan <omonzebaguan.grn@gmail.com>
2026-04-16 20:17:44 +0100

    tested mocked api

--- components/TodoPhotoList.test.tsx
+++ components/TodoPhotoList.test.tsx
@@ -1,5 +1,34 @@
 import "@testing-library/jest-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import TodoAndPhotoList from "@/components/TodoAndPhotoList";
+import { url } from "inspector/promises";
 
-describe("", () => {});
+const originalFetch = global.fetch;
+
+describe("Testing TodoPhotolist component", () => {
+  afterEach(() => {
+    global.fetch = originalFetch;
+  });
+
+  it("renders todo list", async () => {
+    const mockTodos = [
+      { id: 1, title: "Todo 1", completed: false },
+      { id: 2, title: "Todo 2", completed: true },
+    ];
+
+    // ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockTodos),
+      }),
+    );
+    // ts-ignore
+
+    render(<TodoAndPhotoList />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Todo 1 - Pending")).toBeInTheDocument();
+      expect(screen.getByText("Todo 2 - Completed")).toBeInTheDocument();
+    });
+  });
+});
